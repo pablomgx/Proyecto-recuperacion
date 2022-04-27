b81cfd152111926a833a78d049769c92c856ef57
Pablo Míguez <pmiguezg@fpcoruna.afundacion.org>
2022-04-27 09:30:48 +0200

    Fix en el código

--- php/do_login.php
+++ php/do_login.php
@@ -8,7 +8,10 @@
     $query = "SELECT id, password_hashed, num_telefono FROM Usuario WHERE num_telefono = '".$phone."'";
 
     $result = mysqli_query($db, $query) or die(header('Location: login.php?login_failed_unknown=True'));
-    if (mysqli_num_rows($result) > 0) {
+    if (mysqli_num_rows($result) == 0) {
+        header("Location: login.php?login_failed_phone=True");
+        exit;
+    } else {
         $only_row = mysqli_fetch_array($result);
         if (password_verify($password,$only_row[1])) {
             session_start();
